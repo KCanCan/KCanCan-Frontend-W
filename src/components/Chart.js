826610005360d7237b503da6cffdab893714aae7
Wayne Sun <wtse.sun@gmail.com>
2020-09-05 16:03:21 -0700

    installed react-spring

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,11 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import styles from "../styles/Chart.module.css";
 
 export default function Chart({ data }) {
   const [width, setWidth] = useState(500);
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setWidth(parseInt(400 * Math.random() + 100));
+  //     console.log(width);
+  //   }, 1000);
+  // }, [width]);
+
   return (
     <div>
-      <div className={styles.box}></div>
+      <div className={styles.box} style={{ width: width + "px" }}></div>
     </div>
   );
 }
